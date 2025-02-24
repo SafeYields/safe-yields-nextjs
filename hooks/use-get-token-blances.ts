5b95a8ca17508d7ee7bc6d629c180893363aaa8b
JayusJay <atayure@gmail.com>
2025-02-24 16:50:00 +0000

    refactor useGetTokenBalances to utilize useSafeYieldsContract for USDC balance retrieval

--- hooks/use-get-token-blances.ts
+++ hooks/use-get-token-blances.ts
@@ -1,11 +1,12 @@
 'use client';
-import { USDC_CONTRACT } from '@/constants/contracts/contracts';
 import useEthersProvider from '@/services/blockchain/hooks/useEthersProvider';
 import { Contract, ethers } from 'ethers';
 import { useEffect, useState } from 'react';
+import { useSafeYieldsContract } from '@/services/blockchain/safeyields.contracts';
 
 export default function useGetTokenBalances(address: string, txUpdate: number) {
   const provider = useEthersProvider();
+  const {usdc} = useSafeYieldsContract();
 
   const [usdcBalance, setUsdcBalance] = useState(0);
   const [safeBalance, setSafeBalance] = useState(0);
@@ -16,10 +17,9 @@ export default function useGetTokenBalances(address: string, txUpdate: number) {
 
     const getBalances = async () => {
       try {
-        const USDC_CONTRACT_ = USDC_CONTRACT.connect(provider) as Contract;
         // const SAFE_CONTRACT_ = SAFE_CONTRACT.connect(provider) as Contract;
 
-        const usdcBalance_ = await USDC_CONTRACT_.balanceOf(address);
+        const usdcBalance_ = await usdc.balanceOf(address);
         console.log(usdcBalance_);
         setUsdcBalance(Number(ethers.formatUnits(usdcBalance_, 6)));
       } catch (err) {}
