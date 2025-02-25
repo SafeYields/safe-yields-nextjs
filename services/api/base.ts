1858577229da26ebe03afe2952205fd705974d79
JayusJay <atayure@gmail.com>
2025-02-25 19:50:17 +0000

    update API endpoints to use secure HTTPS and remove console log

--- services/api/base.ts
+++ services/api/base.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 import { SupportedChain } from '../blockchain/constants/addresses';
 
 const endPoints = {
-  42161: 'http://176.9.90.92:8084/api/v1/history', //http://176.9.90.92:8086/api/v1/history',
-  747: 'http://176.9.90.92:8085/api/v1/history',
+  42161: 'https://trading-data.alphacube.io:8084/api/v1/history', //http://176.9.90.92:8086/api/v1/history',
+  747: 'https://trading-data.alphacube.io:8084/api/v1/history',
 };
 
 export const getTradeData = async (chainId: SupportedChain) => {
   try {
     const response = await axios.get(endPoints[chainId]);
-    console.log('response: ', response);
+    //console.log('response: ', response);
     return response.data;
   } catch (err) {
     console.error('error getting trade data', err);
