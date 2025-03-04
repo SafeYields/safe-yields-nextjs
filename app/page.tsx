1cedbc1d6ec4c89ea061f0a2760d022f8fc6f99c
mzerone <mohammedzeglam@gmail.com>
2025-03-04 14:47:02 +0200

    fix home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <>
-      <div className='flex items-start justify-start mt-16 px-16 relative min-h-[80vh] w-full gap-16'>
+      <div className='flex items-start justify-between mt-16 px-16 relative min-h-[80vh] w-full gap-16'>
         <h3 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 max-w-md'>
           Emma AI guides you through selecting optimal yield strategies for{' '}
           <span className='text-[#4CFAC7]'>sustainable growth</span>
@@ -13,7 +13,7 @@ export default function Home() {
           width='695'
           height='692'
           alt='safeyields character'
-          className='inline-block absolute left-56 top-4'
+          className='inline-block absolute left-1/2 -translate-x-1/2 top-4'
         />
         <h3 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0 max-w-md self-end'>
           Find <span className='text-[#4CFAC7]'>consistency</span> in the
