776d64fdabef5e57cb85f74aca415067ee12271f
mzerone <mohammedzeglam@gmail.com>
2025-03-20 15:38:41 +0200

    reduce image size.

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           Emma AI guides you through selecting optimal yield strategies for{' '}
           <span className='text-brand-1'>sustainable growth</span>
         </h3>
-        <div className='fixed aspect-[695/692] md:max-w-4xl max-w-lg mx-auto w-full lg:top-1/2 top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4 lg:-translate-y-1/2'>
+        <div className='fixed aspect-[695/692] md:max-w-2xl max-w-lg mx-auto w-full lg:top-1/2 top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4 lg:-translate-y-1/2'>
           <Image
             src='/images/hero.png'
             fill
