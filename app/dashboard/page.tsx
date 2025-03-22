f1bd00e0c4ad7d27b3547db4171cde33c30c8558
mzerone <mohammedzeglam@gmail.com>
2025-03-22 18:38:32 +0200

    fix build.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -139,7 +139,7 @@ function Dashboard() {
           </ToastAction>
         ),
       });
-    } catch (error) {
+    } catch  {
       return toast({
         title: 'Error claiming airdrop',
       });
